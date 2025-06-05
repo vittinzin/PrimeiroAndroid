1a0c26718366a07d4a1e5587b2121d7baa330937
vittinzin <vitoredua7@gmail.com>
2025-06-05 15:58:17 -0300

    recuperar dados sharedPreferences

--- App/app/src/main/java/com/example/app/controller/CursoController.java
+++ App/app/src/main/java/com/example/app/controller/CursoController.java
@@ -1,23 +1,34 @@
 package com.example.app.controller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.wifi.aware.PublishConfig;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.example.app.model.Curso;
+import com.example.app.model.Pessoa;
 
 public class CursoController {
 
-    @NonNull
-    @Override
-    public String toString() {
+    private static final String prefs = "dados_usuario";
+    private SharedPreferences sharedPreferences;
 
-        Log.d("MVC controller", "Curso iniciada ");
-        return super.toString();
+
+    public CursoController (Context context){
+        sharedPreferences = context.getSharedPreferences(prefs, Context.MODE_PRIVATE);
     }
 
-    public void salvar(Curso curso){
-        Log.d("MVC controller","Curso salvo: " + curso.toString());
+    public void salvarCurso (Curso curso){
+        SharedPreferences.Editor listaVip = sharedPreferences.edit();
+        listaVip.putString("CURSO: ", curso.getCursoDesejado());
+        listaVip.apply();
+    }
+
+    public Curso carregarCurso () {
+        String curso = sharedPreferences.getString("CURSO: ", "");
+        return new Curso(curso);
     }
 
 }
