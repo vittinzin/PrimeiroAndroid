20a5a266a165b33f4fe63e78220430ddd4ad394c
vittinzin <vitoredua7@gmail.com>
2025-06-10 14:18:32 -0300

    Confirmar dados salvos

--- App/app/src/main/java/com/example/app/view/MainActivity.java
+++ App/app/src/main/java/com/example/app/view/MainActivity.java
@@ -6,28 +6,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.app.R;
 import com.example.app.controller.PessoaController;
 import com.example.app.controller.CursoController;
 import com.example.app.model.Pessoa;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    int posi = 0;
+    int posicao = 0;
     Spinner etCurso;
     Button btnSalvar, btnLimpar, btnFinalizar;
     EditText etNome, etSobrenome, etTelefone;
     PessoaController pessoaController;
     CursoController cursoController;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
 
         etCurso = findViewById(R.id.etCurso);
 
@@ -56,50 +58,54 @@ public class MainActivity extends AppCompatActivity  {
             Pessoa pessoa = new Pessoa(
                     etNome.getText().toString(),
                     etSobrenome.getText().toString(),
-                    etCurso.getSelectedItem().toString(),
+                    null,
                     etTelefone.getText().toString()
-
             );
 
-            posi = etCurso.getSelectedItemPosition();
-            cursoController = new CursoController(this);
+            int resultado = pessoaController.confirmarPessoa(pessoa);
 
-            cursoController.salvarPosicao(posi);
-            System.out.println(posi);
+            if (resultado == -1) {
+                pessoaController.salvarPessoa(pessoa);
+                pessoa.setCurso(etCurso.getSelectedItem().toString());
+                posicao = etCurso.getSelectedItemPosition();
 
-            pessoaController.salvarPessoa(pessoa);
-            Toast.makeText(this,pessoa.toString(),Toast.LENGTH_SHORT).show();
+                cursoController = new CursoController(this);
+                cursoController.salvarPosicao(posicao);
 
-        });
-
-        btnLimpar.setOnClickListener( v -> {
-                etNome.setText("");
-                etSobrenome.setText("");
-                etTelefone.setText("");
-                etCurso.setSelection(0);
-
+                Toast.makeText(this, pessoa.toString(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Informações incorretas!", Toast.LENGTH_SHORT).show();
                 cursoController.deletarSpinner();
                 pessoaController.deletarPessoa();
+            }
+        });
 
-            Toast.makeText(this, "Pessoa deletada!",Toast.LENGTH_SHORT).show();
+        btnLimpar.setOnClickListener(v -> {
+            etNome.setText("");
+            etSobrenome.setText("");
+            etTelefone.setText("");
+            etCurso.setSelection(0);
+
+            cursoController.deletarSpinner();
+            pessoaController.deletarPessoa();
+
+            Toast.makeText(this, "Pessoa deletada!", Toast.LENGTH_SHORT).show();
         });
 
         btnFinalizar.setOnClickListener(v -> {
-            Toast.makeText(this, "App finalizado!",Toast.LENGTH_SHORT).show();
-                finish();
+            Toast.makeText(this, "App finalizado!", Toast.LENGTH_SHORT).show();
+            finish();
         });
         carregarPessoaSalva();
-
     }
 
-    public void carregarPessoaSalva () {
+    public void carregarPessoaSalva() {
         Pessoa pessoa = pessoaController.carregarPessoa();
         CursoController cursoController = new CursoController(this);
 
-
         etNome.setText(pessoa.getNome());
         etSobrenome.setText(pessoa.getSobrenome());
-        etCurso.setSelection(cursoController.carregarSpiner());
+        etCurso.setSelection(cursoController.carregarCurso());
         etTelefone.setText(pessoa.getTelefone());
 
     }
